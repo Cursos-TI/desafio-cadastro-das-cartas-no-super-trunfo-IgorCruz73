5d6521b39fd1c1fbfcbe113ffffd76d10c715a3e
IgorCruz73 <ygorcruz76@gmail.com>
2025-02-27 11:01:53 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
 
 int main(){
-    printf("Desafio Super Trunfo!\n");
-
-    
-    char estado, código da carta, nome da cidade;
-    int população, pontos turísticos
-    float área, pib
-}
+printf("Desafio Super Trunfo!\n");
