b3defd086bbc9dce851c1ec714cf4968d3a6c57b
IgorCruz73 <ygorcruz76@gmail.com>
2025-02-28 03:20:10 +0000

    Declaração de tipo de variáveis e os nomes

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,3 +3,19 @@
 int main(){
 printf("Desafio Super Trunfo!\n");
 printf("Insira os dados da Carta N°1 \n");
+
+//Variáveis
+int População1;
+int População2;
+float Área1;
+float Área2;
+float PIB1;
+float PIB2;
+int Número1;
+int Número2;
+char Estado1[50];
+char Estado2[50];
+char Código1[50];
+char Código2[50];
+char Nome1[50];
+char Nome2[50];
